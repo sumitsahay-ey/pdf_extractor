2b91ea73538882aa72347c5f8921ce807b5022de
Sumit Sahay <jupyter@inst-builder-debian-11-build-build-ly0tq.us-central1-a.c.gce-image-builder.internal>
2024-08-26 11:39:04 +0000

    added sh content to notebook and fixed env.sh filetype

---
+++ notebooks/Main/.ipynb_checkpoints/env-checkpoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt install tesseract-ocr
+sudo apt install libtesseract-dev
+
+sudo apt-get update
+sudo apt-get install poppler-utils
+
+pip install pycryptodome==3.15.0
+
+pip install pdfminer.six
+pip install pillow-heif==0.3.2
+pip install matplotlib
+pip install unstructured-inference
+pip install unstructured-pytesseract
+pip install tesseract-ocr
+pip install unstructured
